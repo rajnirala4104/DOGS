b86f8b71c39b17259c118375bc27521a83e15997
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2023-07-06 00:12:44 +0530

    setup done

---
+++ random-dogs-generator/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App bg-red-500 ">
+      <h1>fsdfsd</h1>
+    </div>
+  );
+}
+
+export default App;
